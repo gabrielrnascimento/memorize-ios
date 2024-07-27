807e9f6db3994ee89ac42204571ac8faf2ae94e7
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-07-27 15:22:33 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Gabriel Nascimento on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
