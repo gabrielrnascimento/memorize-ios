df18de5837e9c8cd27d3a682f12bceabc4ad1bec
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-09-15 11:05:52 -0300

    refactor: remove assignment 1 code

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -13,8 +13,6 @@ struct EmojiMemoryGameView: View {
     
     private let aspectRatio: CGFloat = 2/3
     
-    @State var selectedTheme: Theme? = .vehicles
-    
     var body: some View {
         NavigationStack {
             VStack {
@@ -38,49 +36,6 @@ struct EmojiMemoryGameView: View {
                 }
         }
     }
-    
-    var themeButtons: some View {
-        HStack(spacing: 50) {
-            ActionButton(
-                label: "Vehicles",
-                symbol: "car.circle",
-                action: updateTheme,
-                theme: .vehicles,
-                selectedTheme: selectedTheme
-            )
-            ActionButton(
-                label: "Plants",
-                symbol: "leaf.circle",
-                action: updateTheme,
-                theme: .plants,
-                selectedTheme: selectedTheme
-            )
-            ActionButton(
-                label: "Foods",
-                symbol: "fork.knife.circle",
-                action: updateTheme,
-                theme: .foods,
-                selectedTheme: selectedTheme
-            )
-        }
-        .padding()
-    }
-    
-    private func updateTheme(to theme: Theme) {
-        selectedTheme = theme
-    }
-}
-
-enum Theme {
-    case vehicles
-    case plants
-    case foods
-}
-
-extension Color {
-    static let vehicleColor = Color(red: 70/255, green: 130/255, blue: 180/255)
-    static let plantColor = Color(red: 34/255, green: 139/255, blue: 34/255)
-    static let foodColor = Color(red: 255/255, green: 99/255, blue: 71/255)
 }
 
 struct CardView: View {
@@ -107,28 +62,6 @@ struct CardView: View {
                 .opacity(card.isFaceUp ? 0 : 1)
         }
         .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
-    }
-}
-
-struct ActionButton: View {
-    let label: String
-    let symbol: String
-    let action: (Theme) -> Void
-    let theme: Theme
-    let selectedTheme: Theme?
-    
-    var body: some View {
-        Button(action: {
-            action(theme)
-        }) {
-            VStack {
-                Image(systemName: symbol)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .imageScale(.large)
-                Text(label)
-            }
-        }
-        .disabled(selectedTheme == theme)
     }
 }
 
